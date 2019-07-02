8fb04712a776d1a235a0cc3d149aae55fa581acb
Bastianvidal1 <bastianvidal1998@gmail.com>
2019-07-01 23:22:14 -0400

    pppppp

--- SitioWeb/Cotizar.aspx.cs
+++ SitioWeb/Cotizar.aspx.cs
@@ -7,8 +7,60 @@ using System.Web.UI.WebControls;
 
 public partial class Cotizar : System.Web.UI.Page
 {
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        //Llamado a los metodos para definir los dropList desde la base de datos
+        if (!IsPostBack)
+        {
+            definirDropListComuna();
+            definirDropListVehiculo();
+        }
+
+    }
+
+
+
+    /// <summary>
+    /// Define los droplist de comuna dentro del formulario
+    /// </summary>
+    public void definirDropListComuna()
+    {
+
+        //Conexión a la BD
+        using (SampleEntityFramework db = new SampleEntityFramework())
+        {
+            var list = db.Comuna;
+
+
+            droplistComuna.Items.Add("-- Seleccionar item --");
+            droplistComunaDestino.Items.Add("-- Seleccionar itme --");
+            foreach (var objComuna in list)
+            {
+                droplistComuna.Items.Add(objComuna.idComuna + ".- " + objComuna.nombre);
+                droplistComunaDestino.Items.Add(objComuna.idComuna + ".- " + objComuna.nombre);
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// Define los droplist de tipos de vehiculos dentro del formulario
+    /// </summary>
+    public void definirDropListVehiculo()
+    {
+
+        //Conexión a la base de datos
+        using (SampleEntityFramework db = new SampleEntityFramework())
+        {
+            var list = db.Vehiculo;
+
+            droplistTCamion.Items.Add("-- Seleccionar item --");
+            foreach (var objVehiculo in list)
+            {
+                droplistTCamion.Items.Add(objVehiculo.idVehiculo + ".- " + objVehiculo.tipo);
+            }
+        }
 
     }
 }
